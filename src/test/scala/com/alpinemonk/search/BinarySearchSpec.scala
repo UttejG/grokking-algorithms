8999bee9f6b2a9c160e5799b4d2b3c295d0b1da7
Uttej Guduru <uguduru.18@gmail.com>
2017-04-22 19:45:57 -0400

    Cleanup searchable tests

--- src/test/scala/com/alpinemonk/search/BinarySearchSpec.scala
+++ src/test/scala/com/alpinemonk/search/BinarySearchSpec.scala
@@ -4,17 +4,22 @@ import org.scalatest.{Matchers, WordSpec}
 
 class BinarySearchSpec extends WordSpec with Matchers {
   "Search function with Binary Search implementation" should {
+    import com.alpinemonk.search.BinarySearch._
+
+    val intList = List(1, 5, 10, 15, 20, 25, 30)
+    val testValues = List(1 , 2, 5, 7, 10, 12 , 15, 17, 20, 22, 25, 27, 30, 35)
+
     "find the element if exists" in {
       import com.alpinemonk.typeclasses.Searchable._
+
+      val result = for(element <- testValues) yield search(intList, element)
+      result shouldBe (1 to 7).flatMap(_ => List(true, false))
+    }
+
+    "find the element if exists using Ops interface" in {
       import com.alpinemonk.typeclasses.Searchable.Ops._
-      import com.alpinemonk.search.BinarySearch._
 
-      val myList = List(1, 5, 10, 15, 20, 25, 30)
-      val valuesToCheck = List(1 , 2, 5, 7, 10, 12 , 15, 17, 20, 22, 25, 27, 30, 35)
-
-      myList.has(15) shouldBe true // Ops interface
-
-      val result = for(element <- valuesToCheck) yield search(myList, element)
+      val result = for(element <- testValues) yield intList.has(element)
       result shouldBe (1 to 7).flatMap(_ => List(true, false))
     }
   }
